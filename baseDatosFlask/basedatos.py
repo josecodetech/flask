76f3dba67d5201925550cc8371d948c1bdf04fa2
Jose Ojeda <paninarotico@gmail.com>
2022-04-30 20:06:45 +0200

    mysql

---
+++ baseDatosFlask/basedatos.py
@@ -0,0 +1,70 @@
+import pymysql
+
+
+def dame_conexion():
+    return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='',
+        db='basedatosflask')
+
+
+def insertar_articulo(nombre, precio):
+    conexion = dame_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "INSERT INTO articulos(nombre,precio) VALUES (%s, %s)", (nombre, precio))
+        conexion.commit()
+        conexion.close()
+
+
+def listar_articulos():
+    conexion = dame_conexion()
+    articulos = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT id, nombre, precio FROM articulos")
+        articulos = cursor.fetchall()
+        conexion.close()
+        return articulos
+
+
+def eliminar_articulo(id):
+    conexion = dame_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute("DELETE FROM articulos WHERE id = %s", (id))
+        conexion.commit()
+        conexion.close()
+
+
+def obtener_articulo(id):
+    conexion = dame_conexion()
+    articulo = None
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "SELECT id, nombre, precio FROM articulos WHERE id = %s", (id))
+        articulo = cursor.fetchone()
+        conexion.close()
+        return articulo
+
+
+def actualizar_articulo(id, nombre, precio):
+    conexion = dame_conexion()
+    with conexion.cursor() as cursor:
+        cursor.execute(
+            "UPDATE articulos SET nombre = %s, precio = %s WHERE id = %s", (nombre, precio, id))
+        conexion.commit()
+        conexion.close()
+
+
+if __name__ == '__main__':
+    # dame_conexion()
+    # articulos=listar_articulos()
+    # print(articulos)
+    insertar_articulo("boli", 1.2)
+    conexion = dame_conexion()
+    articulos = []
+    with conexion.cursor() as cursor:
+        cursor.execute("SELECT id, nombre, precio FROM articulos")
+        articulos = cursor.fetchall()
+        conexion.close()
+    print(articulos)
---
+++ baseDatosFlask/main.py
@@ -0,0 +1,50 @@
+import re
+from flask import Flask, redirect, request, url_for, render_template
+import basedatos
+app = Flask(__name__)
+
+
+@app.route('/agregar_articulo')
+def agregar_articulo():
+    return render_template("agregar_articulo.html")
+
+
+@app.route('/guardar_articulo', methods=['POST'])
+def guardar_articulo():
+    nombre = request.form['nombre']
+    precio = request.form['precio']
+    basedatos.insertar_articulo(nombre, precio)
+    return redirect('/articulos')
+
+
+@ app.route('/')
+@ app.route('/articulos')
+def articulos():
+    articulos = basedatos.listar_articulos()
+    return render_template('articulos.html', articulos=articulos)
+
+
+@ app.route("/eliminar_articulo", methods=['POST'])
+def eliminar_articulo():
+    basedatos.eliminar_articulo(request.form['id'])
+    return redirect("/articulos")
+
+
+@ app.route("/editar_articulo/<int:id>")
+def editar_articulo(id):
+    articulo = basedatos.obtener_articulo(id)
+    return render_template("editar_articulo.html", articulo=articulo)
+
+
+@ app.route("/actualizar_articulo", methods=['POST'])
+def actualizar_articulo():
+    id = request.form["id"]
+    nombre = request.form["nombre"]
+    precio = request.form["precio"]
+    basedatos.actualizar_articulo(id, nombre, precio)
+    return redirect("/articulos")
+
+
+if __name__ == '__main__':
+
+    app.run(debug=True)
